ae6b8e3a5fedbcbf5caa5bce5b77efc576f81113
Deepa Jagan <deepajaganj.13@gmail.com>
2026-07-15 13:39:32 +0530

    Create GCD of Odd and Even Sums.cpp

---
+++ GCD of Odd and Even Sums.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    cout << "Result: " << obj.gcdOfOddEvenSums(n) << endl;
+
+    return 0;
+}
