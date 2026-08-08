79176b9ee6098b239fff9ced9f56a2a16bdccfd9
Deepa Jagan <deepajaganj.13@gmail.com>
2026-08-08 20:16:57 +0530

    Implement function to create smallest palindrome

---
+++ makeSmallestPalindrome.cpp
@@ -0,0 +1,6 @@
+    string makeSmallestPalindrome(string s) {
+        int n = s.size();
+        for (int i = 0; i < n; ++i)
+            s[i] = s[n - 1 - i] = min(s[i], s[n - i - 1]);
+        return s;
+    }
