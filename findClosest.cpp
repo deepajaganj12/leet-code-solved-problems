419ac6f3808a552addb75b1a287a30417f8332e7
Deepa Jagan <deepajaganj.13@gmail.com>
2026-08-12 13:52:17 +0530

    Implement findClosest function in findClosest.cpp

---
+++ findClosest.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int p1=abs(x-z);
+        int p2=abs(y-z);
+        if(p1<p2) return 1;
+        else if(p2==p1) return 0;
+        else return 2; 
+    }
+};
