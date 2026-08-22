524dae9bfc19eb9a356043d03754bc05b19491b7
Deepa Jagan <deepajaganj.13@gmail.com>
2026-08-22 20:58:14 +0530

    Implement DFS to find bottom left value in tree

---
+++ findBottomLeftValue.cpp
@@ -0,0 +1,24 @@
+// 🌳 Using DFS
+class Solution {
+    int maxLevel = -1;
+    int ans = 0;
+
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        value(root, 0);
+        return ans;
+    }
+
+private:
+    void value(TreeNode* root, int level) {
+        if (root == nullptr) return;
+
+        if (level > maxLevel) {
+            maxLevel = level;
+            ans = root->val;
+        }
+
+        value(root->left, level + 1);
+        value(root->right, level + 1);
+    }
+};
