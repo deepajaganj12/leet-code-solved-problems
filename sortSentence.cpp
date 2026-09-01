d6b0b0be09cabbbe2fd3ae1e7075da6ac3a9a75d
Deepa Jagan <deepajaganj.13@gmail.com>
2026-09-01 11:20:49 +0530

    Create sortSentence.cpp

---
+++ sortSentence.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+        vector<string> ans(10);
+        string temp;
+        int i = 0;
+        int count = 0;
+
+        s.push_back(' ');
+        while (i < s.size()) {
+            if (s[i] == ' ') {
+                int pos = temp[temp.size() - 1] - '0';
+                temp.pop_back(); 
+                ans[pos] = temp;
+                temp = "";
+                count++;
+                i++;
+            } else {
+                temp += s[i];
+                i++;
+            }
+        }
+        for (int i = 1; i <= count; i++) {
+            temp += ans[i];
+            temp += " ";
+        }
+        temp.pop_back(); 
+        return temp;
+    }
+};
