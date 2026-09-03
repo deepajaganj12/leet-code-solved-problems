9d52cdb17c477a8d5e31ce4e0b52c39d39a01385
Deepa Jagan <deepajaganj.13@gmail.com>
2026-09-03 09:28:42 +0530

    Implement sum of digits for Harshad number check

---
+++ sumOfTheDigitsOfHarshadNumber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sumNumber = 0;
+        int temp = x;
+        while (temp > 0) {
+            sumNumber += temp % 10;
+            temp /= 10;
+        }
+        if (x % sumNumber == 0)
+            return sumNumber;
+        return -1;
+    }
+};
