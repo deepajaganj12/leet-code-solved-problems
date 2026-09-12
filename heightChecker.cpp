59e1d0f86665127b5dfee27c5c7f793c266506e5
Deepa Jagan <deepajaganj.13@gmail.com>
2026-09-12 09:52:45 +0530

    Create heightChecker.cpp

---
+++ heightChecker.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int>arr = heights;
+        int count=0;
+        sort(arr.begin(),arr.end());
+        for(int i=0 ; i<arr.size();i++){
+            if(arr[i]!=heights[i])count++;
+        }
+        return count;
+    }
+};
